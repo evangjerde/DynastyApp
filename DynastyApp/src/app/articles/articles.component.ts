afb34695e51b666e7f8b8530a794c4083e34aea9
Evan Gjerde <evan@evangjerde.com>
2022-08-24 11:11:39 -0500

    Added articles to s3 (add more later) and links to article section

--- DynastyApp/src/app/articles/articles.component.ts
+++ DynastyApp/src/app/articles/articles.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
   articles: any[] = [
-    { title: 'Dynasty Rankings Preseason 2022', path: '2022psRankings' }
+    { title: 'Dynasty Rankings Preseason 2022', path: '2022psRankings' },
+    { title: 'Dynasty Magazine Issue No. 6', path: 'https://dynasty-app-assets.s3.us-east-2.amazonaws.com/articles/Dynasty_Mag_Issue_6.pdf' },
+    { title: 'Dynasty Magazine Issue No. 5', path: 'https://dynasty-app-assets.s3.us-east-2.amazonaws.com/articles/Dynasty_Mag_Issue_5.pdf' },
+    { title: 'Dynasty Magazine Issue No. 4', path: 'https://dynasty-app-assets.s3.us-east-2.amazonaws.com/articles/Dynasty_Mag_Issue_4.pdf' },
+    { title: 'Dynasty Magazine Issue No. 3', path: 'https://dynasty-app-assets.s3.us-east-2.amazonaws.com/articles/Dynasty_Mag_Issue_3.pdf' },
+    { title: 'Dynasty Magazine Issue No. 2', path: 'https://dynasty-app-assets.s3.us-east-2.amazonaws.com/articles/Dynasty_Mag_Issue_2.pdf' },
+    { title: 'Dynasty Magazine Issue No. 1', path: 'https://dynasty-app-assets.s3.us-east-2.amazonaws.com/articles/Dynasty_Mag_Issue_1.pdf' }
   ];
 
   constructor() { }
